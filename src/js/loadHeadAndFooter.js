1e28170babd8515003b992ee7a6f825985cd7e29
duxiaoqi <393985832@qq.com>
2018-05-19 18:03:43 +0800

    disanci

--- src/js/loadHeadAndFooter.js
+++ src/js/loadHeadAndFooter.js
@@ -3,11 +3,21 @@ define(["jquery","cookie"],function($){
     $(function(){
         /*加载头部*/
         $("header").load("/html/include/header.html",function(){
-
+            $(".zt",".headerz").hover(function(){
+                $(".header1",".headerx").css({"display":"block"});
+            },function(){
+                setTimeout(function(){$(".header1",".headerx").css({"display":"none"})},4000);
+            });
+            $(".fl",".headerz").hover(function(){
+                $(".header2",".headerx").css({"display":"block"});
+            },function(){
+                setTimeout(function(){$(".header2",".headerx").css({"display":"none"})},4000);
+            });
         });
+            
         /*加入尾部*/
         $("footer").load("/html/include/footer.html",function(){
-            
+
         });
     });
 });
